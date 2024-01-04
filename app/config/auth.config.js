1cbc588aa774f374e9f038c77db16ea5b6aa185a
Antonio Massaro <tolf96@gmail.com>
2024-01-04 17:35:24 +0100

    minor

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: "sium-secret-key",
     carRentalClientId: "929370134314-r3bf0hiitqvi6s8hj70kffb11urb8c4a.apps.googleusercontent.com",
-    accessTokenDuration: 10
+    accessTokenDuration: 60
 };
